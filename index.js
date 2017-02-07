853bc36320aa557ab6653dccd8d656fcd23225a9
Priyanka Goyal <osinghal00@gmail.com>
2017-02-07 12:43:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function () {
+    console.log("servers started at port: 3000");
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
+app.get('/login', function (req, res) {
+    res.send('get login');
+});
+
+app.post('/login', function (req, res) {
+    res.send('post login');
+});
